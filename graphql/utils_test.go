ebbcc1569bd4ca8ff82a1701cdf7feb4c2efb71b
Paul Lhussiez <paul.lhussiez@schibsted.com>
2018-10-15 18:22:17 +0200

    Adding comments and cleaner utils

--- graphql/utils_test.go
+++ graphql/utils_test.go
@@ -6,28 +6,28 @@ import (
 
 func TypeInSchema(in []byte, name, description string, extra ...string) bool {
 	var found bool
-	hasExtra := len(extra) > 0
-
 	jsonparser.ArrayEach(in, func(value []byte, dataType jsonparser.ValueType, offset int, err error) {
 		if found {
 			return
 		}
-		in, err := jsonparser.GetString(value, "name")
-		if err != nil || in != name {
+		var in string
+
+		// Break if name is different
+		if in, err = jsonparser.GetString(value, "name"); err != nil || in != name {
 			return
 		}
+		// Break if description is different
 		if in, err = jsonparser.GetString(value, "description"); err != nil || in != description {
 			return
 		}
-		if hasExtra {
-			for _, f := range extra {
-				if _, _, _, err := jsonparser.Get(value, f); err != nil {
-					return
-				}
+		// Break if any of the extra fields can't be found in the type structure
+		for _, f := range extra {
+			if _, _, _, err := jsonparser.Get(value, f); err != nil {
+				return
 			}
 		}
+		// Otherwise set to true
 		found = true
 	}, "__schema", "types")
-
 	return found
 }
